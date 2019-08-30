c268aa8c663c90f46bf531658b1d819a9d7d2713
Jon Ball <jrball3@verizon.net>
2019-08-29 23:21:17 -0400

    fix actions so they dispatch properly

--- screens/create-account/Actions.js
+++ screens/create-account/Actions.js
@@ -1,16 +1,10 @@
-import { selectState } from './Helpers';
-
 export const UPDATE_FIELD = 'UPDATE_FIELD'
-export const updateField = ({ field, value }) => (dispatch, getState) => {
-  console.log(`updateField [${field} = ${value}]`)
-  return {
+export const updateField = ({ field, value }) => 
+  (dispatch) => dispatch({
     type: UPDATE_FIELD,
     field,
     value,
-  }
-}
+  })
 
-export const createAccount = () => (dispatch, getState) => {
-  console.log('Account created!')
-  console.log(selectState(getState()))
-}
+export const createAccount = () => 
+  (dispatch, getState) => dispatch({ type: 'CREATE_ACCOUNT' })
